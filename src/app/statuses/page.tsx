1947fe67e10950aa74f53d768740b0fa5dc5b185
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2025-10-01 22:07:17 +0300

    feat: use statuses.json instead of hardcoded

--- src/app/statuses/page.tsx
+++ src/app/statuses/page.tsx
@@ -1,22 +1,22 @@
 import ServiceStatus from "@/components/serviceStatus";
+import statuses from "./statuses.json"
 
 export default function Statuses() {
-
+    // pull statuses from statuses.json first
+    const serviceStatuses = Object.values(statuses);
 
     return (
         <div className="p-8 max-w-3xl mx-auto mt-16 min-h-screen">
             <h1 className="text-3xl font-bold mb-4">Service Statuses</h1>
             <hr className="border-white/20 mb-8" />
-            <ServiceStatus
-                title="ralseibot"
-                description="A Discord bot for all your Ralsei-related needs, built with TypeScript and Discord.js. "
-                url="https://ralseibot.bnajns.hackclub.app/health"
-            />
-            <ServiceStatus
-                title="Powerbots"
-                description="A simple, yet powerful discord bot, with multiple commands such as mod tools, image manipulation, fun commands, etc."
-                url="https://powerbots.bnajns.hackclub.app/health"
-            />
+            {serviceStatuses.map((status) => (
+                <ServiceStatus
+                    key={status.title}
+                    title={status.title}
+                    description={status.description}
+                    url={status.url}
+                />
+            ))}
         </div>
     )
 }
